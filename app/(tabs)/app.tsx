0752c41d8dc825af3a00a5c5352cb284dc942146
ab1kenobi <107165784+ab1kenobi@users.noreply.github.com>
2025-10-20 12:27:22 -0500

    first commit on my branch

--- app/(tabs)/app.tsx
+++ app/(tabs)/app.tsx
@@ -18,7 +18,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">This is really cool</ThemedText>
+        <ThemedText type="title">Branch 1</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
